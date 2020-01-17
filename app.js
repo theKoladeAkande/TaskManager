9b13f85bcfaa6263a14d35de5bf43ce6e11b2e67
theKoladeAkande <akandekolade@gmail.com>
2020-01-17 10:26:30 +0100

    connect to redis and more functionality: added more functionality connected to redis client index.ejs renders Task list as response to get displayed populated list add functionality with post to push task to task in db

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var bodyParser = require('body-parser');
 var redis = require('redis');
 var app = express();
 
+//Client
+var client = redis.createClient();
+
+client.on('connect', function(){
+    console.log('Redis server started');
+});
+
 // setup view engine
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
@@ -15,8 +22,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response){
-    response.send('Hello Worrld!')
-});
+    var title = 'Task List';
+
+    client.lrange('tasks',0, -1, function(err, reply){
+        response.render('index',{
+            title: title,
+            tasks: reply 
+
+   });// response.render 
+    });//client.lrange 
+
+    
+   
+}); // app.get 
+
+
+app.post('/task/add', function(request,response){
+    var task = request.body.task;
+    client.rpush('tasks', task, function(err, reply){
+        if (err){
+            console.log(err);
+        }//if
+
+        console.log('Task Added');
+        response.redirect('/');
+
+    });//client.rpush
+});//app.post
 
 // start server
 app.listen(3000);
